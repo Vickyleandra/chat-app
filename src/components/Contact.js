b2108e369e6bf43004e94087eabdb553db63fbc7
Vicky-Leandra Teixeira <vickyleandra5@gmail.com>
2020-01-28 17:19:23 +0000

    added component folder

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact(props) {
+    return  (
+    <div className="Contact">
+          <img src={props.avatar} className="avatar"/>
+          <div>
+            <h4 className="name">{props.name}</h4>
+            <div className="status">
+                <div className={props.online ? "status-online" : "status-offline"} />
+                <span className="status-text">
+                    {props.online ? 'Online' : 'Offline'}
+                </span>
+            </div>
+          </div>
+      </div>
+      );      
+}
+
+export default Contact;
